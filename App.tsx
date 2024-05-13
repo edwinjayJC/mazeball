13b10b0abfa9ac2ea158889701a75be6f5709499
Edwin Jay <edwinj@mit.edu>
2024-05-13 06:43:37 +0200

    chore: code cleanup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import MazeBall from './mazeBall';
-import Animated, {
-  withSpring,
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated';
 
 import {
   Accelerometer,
-  Barometer,
-  DeviceMotion,
-  Gyroscope,
-  LightSensor,
-  Magnetometer,
-  MagnetometerUncalibrated,
-  Pedometer,
 } from 'expo-sensors';
 
 Accelerometer.isAvailableAsync().then(result => {
@@ -23,14 +10,6 @@ Accelerometer.isAvailableAsync().then(result => {
 });
 
 export default function App() {
-
-  const width = useSharedValue(100);
-
-  const handlePress = () => {
-    width.value = withSpring(width.value + 50);
-  };
-
-
   return (
     <View style={styles.container}>
     <MazeBall/>
